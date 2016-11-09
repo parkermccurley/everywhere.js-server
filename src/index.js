822c20057a6f3cd9923b52cf5280c898f9e2cbfd
parker mccurley <parkermccurlz@gmail.com>
2016-11-09 01:17:56 -0500

    Refactor Structure

--- src/index.js
+++ src/index.js
@@ -5,15 +5,14 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = 3000;
-
 var router = express.Router();
 
 router.get('/', function(req, res) {
   res.json({ message: 'Hello, world!' });
 });
 
-app.use('/api', router);
+app.use(router);
 
-app.listen(port);
-console.log("Magic happens on port " + port);
+app.listen('3000', function() {
+  console.log("It's alive!");
+});
